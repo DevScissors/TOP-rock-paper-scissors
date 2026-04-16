07bbaad5cb42fb6528b6114ce9cde26b33b7b4ed
DevScissors <sean.scissors@gmail.com>
2026-04-16 16:20:38 -0700

    Beginning to add some style and design to RPS

--- script.js
+++ script.js
@@ -17,44 +17,63 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getHumanChoice() {
-    const humanChoice = window.prompt("Choose, rock, paper, or scissors");
-    return humanChoice;
-}
+// function getHumanChoice() {
+//     const humanChoice = window.prompt("Choose, rock, paper, or scissors");
+//     return humanChoice;
+// }
+
+
 
 function playRound(humanChoice, computerChoice) {
+    const divResults = document.querySelector(".results");
+    const resultParaTie = document.createElement("p");
+    const resultParaWin = document.createElement("p");
+    const resultParaLoss = document.createElement("p");
+
     if (humanChoice.toLowerCase() === computerChoice) {
-        console.log("It's a tie! No one wins.");
-        return console.log("The score is now \n Human: " + humanScore + " \n Computer: " + computerScore);
+        resultParaTie.textContent += "It's a tie! No one wins.\n"
+            + "The score is now \nHuman: " + humanScore + " \nComputer: " + computerScore + "\n";
+        divResults.appendChild(resultParaTie);
     }
     else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors" ||
         humanChoice.toLowerCase() === "paper" && computerChoice === "rock" ||
         humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
         humanScore++;
-        console.log(humanChoice + " beats " + computerChoice + " so you win!");
-        return console.log("The score is now \n Human: " + humanScore + " \n Computer: " + computerScore);
+        resultParaWin.textContent += humanChoice + " beats " + computerChoice + " so you win!\n"
+            + "The score is now \n Human: " + humanScore + " \n Computer: " + computerScore + "\n";
+        divResults.appendChild(resultParaWin);
     } else {
         computerScore++;
-        console.log(computerChoice + " beats " + humanChoice + " you lose!");
-        return console.log("The score is now \n Human: " + humanScore + " \n Computer: " + computerScore);
+        resultParaLoss.textContent += computerChoice + " beats " + humanChoice + " you lose!\n"
+            + "The score is now \n Human: " + humanScore + " \n Computer: " + computerScore + "\n";
+        divResults.appendChild(resultParaLoss);
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    console.log("The final score is \n Human: " + humanScore + " \n Computer: " + computerScore);
-    if (humanScore === computerScore) {
-        return console.log("It's a tie, no one wins!");
-    } else if (humanScore > computerScore) {
-        return console.log("You win!");
-    } else {
-        return console.log("The computer beat you, you lost!");
-    }
+const rockButton = document.querySelector(".rockButton");
+const paperButton = document.querySelector(".paperButton");
+const scissorsButton = document.querySelector(".scissorsButton");
 
-}
+rockButton.addEventListener('click', () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener('click', () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener('click', () => playRound("scissors", getComputerChoice()));
 
-playGame();
+
+// function playGame() {
+//     for (let i = 0; i < 5; i++) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
+//     console.log("The final score is \n Human: " + humanScore + " \n Computer: " + computerScore);
+//     if (humanScore === computerScore) {
+//         return console.log("It's a tie, no one wins!");
+//     } else if (humanScore > computerScore) {
+//         return console.log("You win!");
+//     } else {
+//         return console.log("The computer beat you, you lost!");
+//     }
+
+// }
+
+// playGame();
